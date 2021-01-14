e15f8f88431b55735b45734d65ec84f413d83410
randolphjr7 <randolphjr7@gmail.com>
2021-01-14 12:22:09 -0500

    Update book layout with ranking, image, description and buy button

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
@@ -3,12 +3,17 @@ package com.codepath.bestsellerlistapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
 import com.codepath.bestsellerlistapp.models.BestSellerBook;
 
+import java.text.BreakIterator;
 import java.util.List;
 
 /**
@@ -35,8 +40,19 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
     @Override
     public void onBindViewHolder(final BookViewHolder holder, int position) {
         holder.mItem = books.get(position);
+        // setText does not accept Integers, only String. rank from the json NYT api is an Integer
+        // adding a "" solves this problem
+        holder.mBookRanking.setText("" + books.get(position).rank);
         holder.mBookTitle.setText(books.get(position).title);
         holder.mBookAuthor.setText(books.get(position).author);
+        holder.mBookDescription.setText(books.get(position).description);
+
+        // RA
+        BestSellerBook bestSellerBook = books.get(position);
+        Glide.with(holder.mView)
+                .load(bestSellerBook.bookImageUrl)
+                .centerInside()
+                .into(holder.mBookImage);
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,15 +73,21 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
 
     public class BookViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
+        public final TextView mBookRanking; // RA
         public final TextView mBookTitle;
         public final TextView mBookAuthor;
+        public final TextView mBookDescription; // RA
+        public final ImageView mBookImage; // RA
         public BestSellerBook mItem;
 
         public BookViewHolder(View view) {
             super(view);
             mView = view;
+            mBookRanking = (TextView) view.findViewById(R.id.ranking); // RA
             mBookTitle = (TextView) view.findViewById(R.id.book_title);
             mBookAuthor = (TextView) view.findViewById(R.id.book_author);
+            mBookDescription = (TextView) view.findViewById(R.id.book_description); // RA
+            mBookImage = (ImageView) view.findViewById(R.id.book_image); // RA
         }
 
         @Override
